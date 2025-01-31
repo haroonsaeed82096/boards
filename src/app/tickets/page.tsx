0df5d73871617cd95a09e9c75ef4d7cc07e8e3a4
Haroon Saeed <haroon.saeed@inter-connect.world>
2025-01-31 12:28:26 +0100

    card component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,22 +2,18 @@ import { Heading } from "@/components/heading";
 import { Suspense } from "react";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import Spinner from "@/components/spinner";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import CreateTicketForm from "@/features/ticket/components/create-ticket-form";
-
+import { CardCompact } from "@/components/card-compact";
 const TicketsPage = async () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader>
-          <CardTitle>Create Ticket</CardTitle>
-          <CardDescription>A new ticket will be created!</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <CreateTicketForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        title="Create Ticket"
+        description="Create a new ticket"
+        className="w-full max-w-[420px] self-center"
+        content={<CreateTicketForm />}
+      />
       <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
